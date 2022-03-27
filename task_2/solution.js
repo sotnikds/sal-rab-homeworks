f87e59b4b31863b89970714b98c98efa62d868c2
sotnikds <diman.samara2016@yandex.ru>
2022-03-27 13:37:25 +0400

    re Done task 2

--- task_2/solution.js
+++ task_2/solution.js
@@ -9,17 +9,15 @@ function calcShipping(sum, min, shipping) {
     let shippingSum;
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
-    if(productsSum==0){
-        shippingSum=0;
-    }
+   
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
-    else if(productsSum>=freeShippingMinSum){
+    if(productsSum==0 || productsSum >= freeShippingMinSum){
         shippingSum=0;
     }
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
-    else if(0<productsSum<freeShippingMinSum){
+    else if(productsSum > 0 && productsSum < freeShippingMinSum){
         shippingSum=shippingPrice;
     }
     // Конец решения задания №2.1.
@@ -35,15 +33,11 @@ function calcDiscount(sum, min, discount) {
     // Задание №2.2. Рассчитать скидку
 
     // создайте переменную discountSum
-    let discountSum;
+    
     // если productsSum больше или равно discountMinSum,
     // то присвойте discountSum значение discountPart процентов от productsSum,
     // иначе присвойте discountSum значение 0
-    if(productsSum>=discountMinSum){
-        discountSum=productsSum*100/discountPart;
-    }else{
-        discountSum=0;
-    }
+    let discountSum = productsSum >= discountMinSum ? productsSum / 100 * discountPart : 0;
     // Конец решения задания №2.2.
 
     return discountSum;
